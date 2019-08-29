640557ddc1d0c345a613dea9eb5a1a3377abadaa
zhly0 <864051262@qq.com>
2019-08-29 15:08:49 +0800

    Update tools.cpp
    a bug fixed

--- source/tools.cpp
+++ source/tools.cpp
@@ -1,3 +1,4 @@
+
 #include "tools.h"
 
 void nms_cpu(std::vector<Anchor>& boxes, float threshold, std::vector<Anchor>& filterOutBoxes) {
@@ -24,18 +25,37 @@ void nms_cpu(std::vector<Anchor>& boxes, float threshold, std::vector<Anchor>& f
         for(unsigned i = 1; i < tmp.size(); i++)
         {
             int tmp_i = tmp[i];
-            float inter_x1 = std::max( boxes[good_idx][0], boxes[tmp_i][0] );
-            float inter_y1 = std::max( boxes[good_idx][1], boxes[tmp_i][1] );
-            float inter_x2 = std::min( boxes[good_idx][2], boxes[tmp_i][2] );
-            float inter_y2 = std::min( boxes[good_idx][3], boxes[tmp_i][3] );
+            /*float inter_x1 = std::max<float>( boxes[good_idx][0], boxes[tmp_i][0] );
+            float inter_y1 = std::max<float>( boxes[good_idx][1], boxes[tmp_i][1] );
+            float inter_x2 = std::min<float>( boxes[good_idx][2], boxes[tmp_i][2] );
+            float inter_y2 = std::min<float>( boxes[good_idx][3], boxes[tmp_i][3] );
 
-            float w = std::max((inter_x2 - inter_x1 + 1), 0.0F);
-            float h = std::max((inter_y2 - inter_y1 + 1), 0.0F);
+            float w = std::max<float>((inter_x2 - inter_x1 + 1), 0.0F);
+            float h = std::max<float>((inter_y2 - inter_y1 + 1), 0.0F);
 
             float inter_area = w * h;
             float area_1 = (boxes[good_idx][2] - boxes[good_idx][0] + 1) * (boxes[good_idx][3] - boxes[good_idx][1] + 1);
-            float area_2 = (boxes[tmp_i][2] - boxes[tmp_i][0] + 1) * (boxes[tmp_i][3] - boxes[tmp_i][1] + 1);
-            float o = inter_area / (area_1 + area_2 - inter_area);           
+            float area_2 = (boxes[tmp_i][2] - boxes[tmp_i][0] + 1) * (boxes[tmp_i][3] - boxes[tmp_i][1] + 1);*/
+
+			float inter_x1 = std::max<float>(boxes[good_idx][0], boxes[tmp_i][0]);
+			float inter_y1 = std::max<float>(boxes[good_idx][1], boxes[tmp_i][1]);
+			float inter_x2 = std::min<float>(boxes[good_idx][0]+ boxes[good_idx][2], boxes[tmp_i][0] + boxes[tmp_i][2]);
+			float inter_y2 = std::min<float>(boxes[good_idx][1] + boxes[good_idx][3], boxes[tmp_i][1] + boxes[tmp_i][3]);
+
+			float w = std::max<float>((inter_x2 - inter_x1 + 1), 0.0F);
+			float h = std::max<float>((inter_y2 - inter_y1 + 1), 0.0F);
+
+			float inter_area = w * h;
+			float area_1 = (boxes[good_idx][2]) * (boxes[good_idx][3] );
+			float area_2 = (boxes[tmp_i][2] ) * (boxes[tmp_i][3]);
+
+			float un = (area_1 + area_2 - inter_area);
+			float o = 0.0;
+			
+            if (un > DBL_EPSILON)
+			{
+				o = inter_area / un;
+			}
             if( o <= threshold )
                 idx.push_back(tmp_i);
         }
